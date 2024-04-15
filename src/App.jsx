bfceef1e9ea181a9ca6156e8cfe7bab35559e3a2
Enyinnaya <Ikedichi.Enyinnaya@Shell.com>
2024-04-15 16:35:10 +0100

    updates to the source code and README.md files

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,40 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [open, setOpen] = useState(true);
+  const Menus = [
+    { title: "Dashboard", src: "Chart_fill" },
+    { title: "Inbox", src: "Chat" },
+    { title: "accounts", src: "User", gap: true },
+    { title: "Schedule", src: "Calendar" },
+    { title: "Search", src: "Search" },
+    { title: "Analytics", src: "Chart" },
+    { title: "Files", src: "Folder", gap: true },
+    { title: "Setting", src: "Setting" },
+  ];
+
   return (
     <div className='flex'>
-      <div className={`${open ? "w-72" : "w-20" } duration-300 h-screen bg-dark-purple relative`}>
+      <div className={`${open ? "w-72" : "w-20" } duration-300 h-screen p-5 pt-8 bg-dark-purple relative`}>
         <img 
           src="./src/assets/control.png" 
-          className={`absolute cursor-pointer bg-white rounded-full -right-3 top-9 w-7 border-2 border-dark-purple ${!open && 'rotate-180'}`}
+          className={`absolute cursor-pointer rounded-full -right-3 top-9 w-7 border-2 border-dark-purple ${!open && 'rotate-180'}`}
           onClick={() => setOpen(!open)}
         />
         <div className='flex gap-x-4 items-center'>
           <img 
             src="./src/assets/logo.png" 
-            className={`cursor-pointer duration-500 w-10 h-10`}
+            className={`cursor-pointer duration-500 w-10 h-10 ${open && "rotate-[360deg]"}`}
           />
-          <h1 className={`text-white origin-left font-medium text-xl duration-300`}>Designer</h1>
+          <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!open && "scale-0"}`}>Designer</h1>
         </div>
+        <ul className='pt-6'>
+          {Menus.map((menu,index) => (
+            <li key={index} className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md ${menu.gap ? "mt-9" : "mt-2"} ${index === 0 && "bg-light-white"}`}>
+              <img src={`./src/assets/${menu.src}.png`}/>
+              <span className={`${!open && 'hidden'} origin-left duration-200`}>{menu.title}</span>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className='p-7 text-2xl font-semibold flex-1 h-screen'>
         <h1>Home Page</h1>
